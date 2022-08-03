77bdc0cfef17f274980ad0258b43e2e688e984a5
Felipe Eduardo <110066769+fhelipe-eduardo@users.noreply.github.com>
2022-08-03 14:59:51 -0300

    feat: fictitious fullstack project

---
+++ script.js
@@ -0,0 +1,26 @@
+function submitMessage () {
+    let nome = document.getElementById("name").value;
+    let telefone = document.getElementById("telephone").value;
+    let assunto = document.getElementById("subject").value;
+    let msg = document.getElementById("msg").value;
+
+    sendEmail(nome, telefone, assunto, msg)
+
+    alert (nome + " Sua mensagem foi enviada com sucesso!")
+}
+
+function sendEmail(nome, telefone, assunto, msg) {
+    var params = {
+        nome: nome,
+        telefone: telefone,
+        assunto: assunto,
+        msg: msg
+    }
+}
+
+emailjs.send("italian_bistro_service", "template_jfm61fa", params)
+    .then(function(response) {
+        console.log("SUCESS!!", response.status, response.text)
+    }, function(error) {
+        console.log("ERROR!!!", error);
+    })
